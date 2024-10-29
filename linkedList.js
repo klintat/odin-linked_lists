7915093e0ba34790eafa04f3fcaea3fc2e3a902d
klintat <98770096+klintat@users.noreply.github.com>
2024-10-29 16:37:31 +0200

    Assignment done

--- linkedList.js
+++ linkedList.js
@@ -121,21 +121,4 @@ class LinkedList {
     }
 }
 
-const list = new LinkedList();
-
-list.append("dog");
-list.append("cat");
-list.prepend("mice");
-list.append("parrot");
-list.prepend("hamster");
-list.pop();
-list.append("snake");
-list.prepend("turtle");
-list.append("spider");
-console.log(list.toString());
-console.log(list.find("spider"));
-console.log(list.find("sss"));
-console.log(list.find("parrot"));
-console.log(list.find("turtle"));
-
-// export { LinkedList };
+export { LinkedList };
--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-// import { LinkedList } from "./linkedList.js";
+import { LinkedList } from "./linkedList.js";
 
-// const list = new LinkedList();
+const list = new LinkedList();
 
+list.append("dog");
+list.append("cat");
+list.prepend("mice");
+list.append("parrot");
+list.prepend("hamster");
+list.pop();
+list.append("snake");
+list.prepend("turtle");
+list.append("spider");
+console.log(list.toString());
+console.log(list.find("spider"));
+console.log(list.find("sss"));
+console.log(list.find("parrot"));
+console.log(list.find("turtle"));
