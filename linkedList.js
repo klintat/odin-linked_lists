0783d7657dba8bc262176498ea9c5d20fcc87fc5
klintat <98770096+klintat@users.noreply.github.com>
2024-10-29 15:46:01 +0200

    Created function to return the total number of the nodes in the list

--- linkedList.js
+++ linkedList.js
@@ -41,5 +41,32 @@ class LinkedList {
 		finalString += `( ${tempNode.value} ) -> null`;
 		return finalString;
 	}
+
+    get size() {
+		let count = 0;
+		let tempNode = this.head;
+		while (tempNode) {
+			count += 1;
+			tempNode = tempNode.nextNode;
+		}
+		return count;
+	}
 }
 
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.prepend("mice");
+list.append("parrot");
+list.prepend("hamster");
+// list.pop();
+list.append("snake");
+list.prepend("turtle");
+list.append("spider");
+console.log(list.toString());
+console.log(list.find("spider"));
+console.log(list.find("sss"));
+console.log(list.find("parrot"));
+console.log(list.find("turtle"));
+// export { LinkedList };
