e5dd0b64300a0ea21d90c00d1c86e06fc6187873
klintat <98770096+klintat@users.noreply.github.com>
2024-10-29 13:06:51 +0200

    Created two classes and function to append node to the list

---
+++ linkedList.js
@@ -0,0 +1,26 @@
+class Node {
+    constructor() {
+        this.value = null
+        this.nextNode = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        if(!this.head) {
+            this.head = new Node();
+            this.head.value = value;
+        } else {
+            const newNode = new Node();
+            newNode.value = value;
+            let temp = thos.head;
+            while (temp.nextNode) temp = temp.nextNode;
+            temp.nextNode = newNode;
+        }
+    }
+}
+
